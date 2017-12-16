09b7815eafb90c0bec0d8334146e4e9dd4135e50
thorbw <thorbw@gmail.com>
2017-12-16 03:56:34 +0100

    first try playing with sass

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import '../../styles/NavBar.css';
+
+
 
 const OuterNav = styled.div `
   text-align: center;
@@ -19,6 +22,7 @@ margin-right: 1em;
 
 
 const NavBar = () => (
+  <div>
 <OuterNav>
   <nav className="navbar"> 
     <a><Link to="/about">Association Logo</Link></a>
@@ -31,6 +35,8 @@ const NavBar = () => (
     </Ul>
   </nav>
 </OuterNav>
+  <p className="test-sass-link">I am styled red with sass</p>
+  </div>
 );
 
 export default NavBar;
