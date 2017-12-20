de8389cea5e1d99418ac2b268ae70d4c76864427
thorbw <thorbw@gmail.com>
2017-12-20 01:28:24 +0100

    cleaned the code but link hover not working here

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,57 +1,8 @@
 import React from 'react';
-
-// import React, { Component } from 'react';
-
 import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
 import '../../styles/NavBar.css';
 
-// const OuterNav = styled.div `
-//   text-align: center;
-//   display:flex;
-//   flex-direction: row;
-//   justify-content: center;
-//   align-items: center;
-// `;
 
-// const Ul = styled.ul `
-//   list-style: none;
-//   margin: 0;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-//   height: 100%;
-// `;
-
-// const Li = styled.li`
-//   padding: 1em 0;
-//   height: 100%;
-
-//   > a {
-//     padding: 1em;
-//     text-decoration: none;
-//     color: #f2f2f2;
-
-//     &:hover {
-//       background-color: #00c2bb;
-//     }
-//   }
-// `;
-
-// const Logo = styled.a`
-//   > a {
-//     text-decoration: none;
-//     color: #00c2bb;
-
-//     &:hover {
-//       color: #f2f2f2;
-//     }
-//   }
-// `
-
-
-  // <li className={this.props.index === 
-  //   this.props.activeIndex ? "current-slide current-slide-active" : "current-slide"}>
 
 class NavBar extends React.Component {
     constructor(props) {
@@ -70,18 +21,18 @@ class NavBar extends React.Component {
     }
     render() {
         return (
-           <div class="navbar">
-            <div class="contaier">
+           <div className="navbar">
+            <div className="contaier">
 
-              <div class="navbar-header">
-                <button class="toggle" onClick={this.clickLogoButton}><span></span>
+              <div className="navbar-header">
+                <button className="toggle" onClick={this.clickLogoButton}><span></span>
                 <span></span>
                 <span></span></button>
-               <Link to="/about"><a href="" class="navbar-brand">Neighborhood Association</a></Link>
+               <Link to="/about" className="navbar-brand">Neighborhood Association</Link>
 
               </div>
 
-                <ul class={this.state.showMenu}>
+                <ul className={this.state.showMenu}>
                   <li><Link to="/about">About</Link></li>
                   <li><Link to="/community">Community</Link></li>
                   <li><Link to="/news">Ivy City News</Link></li>
@@ -94,26 +45,6 @@ class NavBar extends React.Component {
         );
     }
 }
-
-
-// const NavBar = () => (
-//   <div>
-//     <OuterNav>
-//       <nav className="navbar">
-//         <Logo><Link to="/about">Neighborhood Association</Link></Logo>
-//         <Ul>
-//           <Li><Link to="/about">About</Link></Li>
-//           <Li><Link to="/community">Community</Link></Li>
-//           <Li><Link to="/news">Ivy City News</Link></Li>
-//           <Li><Link to="/events">Events</Link></Li>
-//           <Li><Link to="/donate">Donate</Link></Li>
-//         </Ul>
-//       </nav>
-//     </OuterNav>
-//     <NewNavBar />
-//   </div>
-// );
-
 
 
 
